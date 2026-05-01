8339685f161a9b2448f42b8530842eb3e3516d33
Guiguizin17 <163371194+Guiguizin17@users.noreply.github.com>
2026-05-01 18:25:42 -0300

    icons

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+
+function mudouTamanho() {
+    if (window. innerWidth >= 768) {
+        items.style.display = 'block'
+    }else {
+        items.style.display = 'block'
+    }
+}
+
+function clickMenu() {
+    if (items.style.display == 'block') {
+        items.style.display = 'none'
+    } else {
+        items.style.display = 'block'
+    }
+}
