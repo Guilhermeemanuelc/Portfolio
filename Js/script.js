b2163900c454c41c41983d621642745ca21ba61f
Guiguizin17 <163371194+Guiguizin17@users.noreply.github.com>
2026-05-04 21:13:59 -0300

    ...

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,23 @@
-
-function mudouTamanho() {
-    if (window. innerWidth >= 768) {
-        items.style.display = 'block'
-    }else {
-        items.style.display = 'block'
-    }
-}
-
 function clickMenu() {
     if (items.style.display == 'block') {
         items.style.display = 'none'
+        burguer.innerHTML = 'menu'
     } else {
         items.style.display = 'block'
+        burguer.innerHTML = 'close'
     }
+
+window.addEventListener("load", () => {
+  const hash = window.location.hash;
+
+  if (hash) {
+    const el = document.querySelector(hash);
+
+    if (el) {
+      setTimeout(() => {
+        el.scrollIntoView();
+      }, 200);
+    }
+  }
+});
 }
