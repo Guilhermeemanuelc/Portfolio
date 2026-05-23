837bc5d76ad7c3527d473f4ccacffeedc9a2fea9
Guiguizin17 <163371194+Guiguizin17@users.noreply.github.com>
2026-05-23 17:20:11 -0300

    ...

--- Js/script.js
+++ Js/script.js
@@ -20,7 +20,7 @@ navLinks.forEach(link => {
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
       const headerHeight = document.querySelector('header').offsetHeight;
-      const targetPosition = target.offsetTop - headerHeight - -10;
+      const targetPosition = target.offsetTop - headerHeight - -32;
       window.scrollTo({
         top: targetPosition,
         behavior: 'smooth'
