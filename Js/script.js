75e586251d02024d8ad7d17db16800e2a4c82dd5
Guiguizin17 <163371194+Guiguizin17@users.noreply.github.com>
2026-05-25 06:43:43 -0300

    ...

--- Js/script.js
+++ Js/script.js
@@ -4,19 +4,28 @@ const burguer = document.getElementById('burguer')
 const menu = document.getElementById('menu')
 const close = document.getElementById('close')
 const links = document.querySelectorAll('.link')
+const overlay = document.getElementById('overlay')
 
 burguer.addEventListener('click', () => {
   menu.classList.add('active')
+  overlay.classList.add('active')
 })
 
 close.addEventListener('click', () => {
   menu.classList.remove('active')
+  overlay.classList.remove('active')
 })
 
 links.forEach(link => {
   link.addEventListener('click', () => {
     menu.classList.remove('active')
+    overlay.classList.remove('active')
   })
+
+overlay.addEventListener('click', () => {
+  menu.classList.removee('active')
+  overlay.classList.remove('active')
+})
 })
 
 
