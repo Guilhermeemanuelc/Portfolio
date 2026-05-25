4adfee654cc244b7cfe715239a764acdbb9d10c1
Guiguizin17 <163371194+Guiguizin17@users.noreply.github.com>
2026-05-24 23:49:31 -0300

    .....................................

--- Js/script.js
+++ Js/script.js
@@ -21,14 +21,14 @@ links.forEach(link => {
 
 
 // Scroll suave para links de navegação
-const navLinks = document.querySelectorAll('.menu ul a.link');
+const navLinks = document.querySelectorAll('#menu ul a.link');
 navLinks.forEach(link => {
   link.addEventListener('click', function(e) {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
       const headerHeight = document.querySelector('header').offsetHeight;
-      const targetPosition = target.offsetTop - headerHeight - -32;
+      const targetPosition = target.offsetTop - headerHeight - -33;
       window.scrollTo({
         top: targetPosition,
         behavior: 'smooth'
